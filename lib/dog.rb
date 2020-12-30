195ce2ee7573bf7d23fb0ae96c178794ee4b3c66
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 19:21:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,16 @@ class Dog
     new_dog
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 
 
 
