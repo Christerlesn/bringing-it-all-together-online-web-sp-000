c1a0e7d11852c4fe066faad693bf75a188510d1a
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 19:46:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,12 +61,12 @@ class Dog
 
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-      if !dog.empty?
-        dog_info = dog[0]
-        dog = Dog.new(id:dog_info[0], name:dog_info[1], breed:dog_info[2])
-      else
-        dog = dog.create(name: name, breed:breed)
-      end
+    if !dog.empty?
+      dog_info = dog[0]
+      dog = Dog.new(id:dog_info[0], name:dog_info[1], breed:dog_info[2])
+    else
+      dog = dog.create(name: name, breed:breed)
+    end
   end
 
   def update
