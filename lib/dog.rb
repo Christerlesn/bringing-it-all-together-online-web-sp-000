ea0b2aded53645e3fcd636cade120c62b674ee03
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 19:43:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,7 +65,7 @@ class Dog
         dog_info = dog[0]
         dog = Dog.new(id:dog_info[0], name:dog_info[1], breed:dog_info[2])
       else
-        dog.create(name:name, breed:breed).unique
+        dog.create(name:name, breed:breed)
       end
       dog
   end
